e3f3a08222b526329d3cb5659fef1e5a35df165a
andrewzee23 <andrewazamora@gmail.com>
2020-11-25 19:59:43 -0600

    calculating bill with tip

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ print('Welcome to the TIPZZ CAL')
 bill = float(input('What was the total bill? $'))
 tip = int(input('What % of tip would you like to give? 10, 15, or 20? '))
 persons = int(input('How many people are paying? '))
+
+total_bill = tip / 100 * bill + bill
