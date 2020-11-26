fe0ef00dd8cfe0331697acea6b249cd61d9ce392
andrewzee23 <andrewazamora@gmail.com>
2020-11-25 20:00:12 -0600

    caluclating bill per person

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ tip = int(input('What % of tip would you like to give? 10, 15, or 20? '))
 persons = int(input('How many people are paying? '))
 
 total_bill = tip / 100 * bill + bill
+bill_per_persons = total_bill / persons
